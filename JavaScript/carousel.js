de653e8b6a5b52e2df6cb7471378304276f9a608
Mateus <mateus.saraiva09@gmail.com>
2023-03-14 03:05:06 -0300

    carrousel concluid

--- JavaScript/carousel.js
+++ JavaScript/carousel.js
@@ -1,12 +1,35 @@
-const controls = document.querySelectorAll("control");
-const item = document.querySelectorAll(".imgsProjects");
+const controls = document.querySelectorAll(".control");
+const items = document.querySelectorAll(".imgsProjects");
 
-const maxItens = item.length;
+const maxItens = items.length;
 
-let currentItem;
+let currentItem = 0;
+
+
 
 controls.forEach(control => {
-    control.addEventListener('click', () => {
-        console.log('control')
-    });
+    function test() {
+        const isLeft = control.classList.contains('arrowLeft')
+
+        if (isLeft) {
+            currentItem -= 1
+        } else {
+            currentItem += 1
+        }
+
+        if (currentItem >= maxItens) {
+            currentItem = 0
+        }
+
+        if (currentItem < 0) {
+            currentItem = maxItens - 1
+        }
+
+        items[currentItem].scrollIntoView({
+            inline: "center",
+            behavior: "smooth"
+        })
+    }
+    control.addEventListener('click', test)
 });
+
